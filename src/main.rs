faf38dcc059d0de759e7651948f19d5a909cf897
dugi <dugi@channel.io>
2025-01-19 14:13:49 +0900

    feat: try resources

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,19 @@ struct Name(String);
 
 fn main() {
     App::new()
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .add_plugins(DefaultPlugins)
+        .add_plugins(HelloPlugin)
         .run();
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, (update_people, greet_people).chain());
+    }
 }
 
 fn add_people(mut commands: Commands) {
@@ -17,13 +27,14 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Jane Doe".to_string())));
 }
 
-fn hello_world() {
-    println!("Hello, world!");
-}
+#[derive(Resource)]
+struct GreetTimer(Timer);
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("Hello, {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("Hello, {}!", name.0);
+        }
     }
 }
 
